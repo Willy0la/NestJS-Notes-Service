e868c86e36fc8756475006df6d00f2aa85f0aba0
Adenuga Willy <adenugawilly431@gmail.com>
2025-08-09 21:40:32 +0100

    config-fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,35 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { NotesModule } from './notes/notes.module';
-
+import * as Joi from 'joi';
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: ['development', 'local', 'staging'],
+      envFilePath: ['.env', '.development', '.staging', '.local'],
       isGlobal: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'staging', 'production', 'local')
+          .required(),
+        DB: Joi.string().required(),
+        PORT: Joi.number().default(3000),
+      }),
+      validationOptions: {
+        abortEarly: true,
+        allowUnknown: true,
+      },
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('DB'),
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }),
+      inject: [ConfigService],
     }),
     UsersModule,
     NotesModule,
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Connection } from 'mongoose';
+import { getConnectionToken } from '@nestjs/mongoose';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const config = app.get(ConfigService);
+  const connection = app.get<Connection>(getConnectionToken());
+  connection.once('connected', () => {
+    console.log('MongoDB connection established successfully');
+  });
+  const port = config.get<number>('PORT', 2398);
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
+
+  console.log('process.env.DB:', process.env.DB);
+  console.log('process.env.NODE_ENV:', process.env.NODE_ENV);
+
   app.setGlobalPrefix('note-app');
 }
 void bootstrap();
